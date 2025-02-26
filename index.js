2157c0dfe934961e73246fda47bce7b6049213ea
mbxisbankai <roy.mbui@student.moringaschool.com>
2025-02-26 10:27:37 +0300

    Looping Lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(array, event){
+    const messages = [];
+    for (let i = 0; i < array.length; i++){
+        messages.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+}
+
+function countDown(number){
+    let i = 0;
+    while(i <= number){
+        console.log(number);
+        number--;
+    }
+}
